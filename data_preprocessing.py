fa2beed7258e5ab5474a523e3fc0e1d7e7faffff
bigballon <605718199@qq.com>
2017-08-03 21:08:06 +0800

    WResNet

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -13,25 +13,25 @@ train_pic_val_path = "./train_ima_val/"
 
 def get112image(img_path):
     img = Image.open(img_path)
-    ori_w,ori_h = img.size
-    new_w = 224.0;
-    new_h = 224.0;
-    if ori_w > ori_h:
-        bs = 224.0 / ori_h;
-        new_w = ori_w * bs
-        weight = int(new_w)
-        height = int(new_h)
-        img = img.resize( (weight, height), Image.BILINEAR )
-        region = ( weight / 2 - 112, 0, weight / 2 + 112, height)
-        img = img.crop( region )
-    else:
-        bs = 224.0 / ori_w;
-        new_h = ori_h * bs
-        weight = int(new_w)
-        height = int(new_h)
-        img = img.resize( (weight, height), Image.BILINEAR )
-        region = ( 0, height / 2 - 112 , weight, height / 2 + 112  )
-        img = img.crop( region )
+    # ori_w,ori_h = img.size
+    # new_w = 224.0;
+    # new_h = 224.0;
+    # if ori_w > ori_h:
+    #     bs = 224.0 / ori_h;
+    #     new_w = ori_w * bs
+    #     weight = int(new_w)
+    #     height = int(new_h)
+    #     img = img.resize( (weight, height), Image.BILINEAR )
+    #     region = ( weight / 2 - 112, 0, weight / 2 + 112, height)
+    #     img = img.crop( region )
+    # else:
+    #     bs = 224.0 / ori_w;
+    #     new_h = ori_h * bs
+    #     weight = int(new_w)
+    #     height = int(new_h)
+    #     img = img.resize( (weight, height), Image.BILINEAR )
+    #     region = ( 0, height / 2 - 112 , weight, height / 2 + 112  )
+    #     img = img.crop( region )
     img = img.resize( (112, 112), Image.BILINEAR )
 #     x = np.array( img, dtype = 'float32')
     x = np.array(img)
@@ -55,8 +55,7 @@ with open("./shuf_train.txt", "r") as f:
         cnt = cnt + 1
         if cnt % 500 == 0:
             print("images: %d, name = %s, class_id = %s" %(cnt,name,class_id))
-        if cnt == 2000:
-            break;
+
         
 print("images: %d" %(cnt))
 elapsed = time.time() - start
@@ -90,8 +89,7 @@ with open("./shuf_train_val.txt", "r") as f:
         cnt = cnt + 1
         if cnt % 500 == 0:
             print("images: %d, name = %s, class_id = %s" %(cnt,name,class_id))
-        if cnt == 2000:
-            break;
+
 print("images: %d" %(cnt))
 elapsed = time.time() - start
 print("Time taken: %.3f seconds." %(elapsed))
@@ -99,7 +97,7 @@ print("Time taken: %.3f seconds." %(elapsed))
 train_list = np.array(train_list,dtype = 'float32')
 y_list = np.array(y_list,dtype = 'float32')
 with open("train_x", "wb") as fp:   #Pickling
-    pickle.dump(train_list, fp)
+    pickle.dump(train_list, fp, protocol=pickle.HIGHEST_PROTOCOL)
 
 with open("train_y", "wb") as fp:   #Pickling
-    pickle.dump(y_list, fp)
+    pickle.dump(y_list, fp, protocol=pickle.HIGHEST_PROTOCOL)
